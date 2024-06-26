8c591d52f5e8ab64bfd8507afa8f39bd3293cdf8
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2024-06-26 12:08:30 +0530

    completed addition of all emelents in the website

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     const btn = document.querySelector('.work-with-me-btn-hover'); // The button
-    const LandingSection = document.getElementById('landing-section'); // The "About Me" section
+    const LandingSection = document.getElementById('landing'); // The "About Me" section
+    const ExperienceSection = document.getElementById('experience'); // The "Experience" section
 
     const offsetToShowButton = LandingSection.offsetHeight + LandingSection.offsetTop;
+    const offsetToHideButton = ExperienceSection.offsetHeight + ExperienceSection.offsetTop;
+
+    console.log("Scroll Position:", window.pageYOffset, "Show Button At:", offsetToShowButton, "Hide Button At:", offsetToHideButton);
 
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset > offsetToShowButton) {
+        if (window.pageYOffset > offsetToShowButton && window.pageYOffset < offsetToHideButton) {
             btn.classList.add('show-work-with-me-btn');
         } else {
             btn.classList.remove('show-work-with-me-btn');
